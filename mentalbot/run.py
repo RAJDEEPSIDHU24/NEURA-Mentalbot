071812dad89131619a0fbf6f6830716186f3b051
RAJDEEPSIDHU24 <rajdeepsidhu642@gmail.com>
2025-05-29 17:28:19 +0530

    Update run.py

--- mentalbot/run.py
+++ mentalbot/run.py
@@ -8,8 +8,7 @@ load_dotenv()
 # Create the app
 app = create_app()
 
-# Print MAIL_DEFAULT_SENDER after app creation
-# print(f"MAIL_DEFAULT_SENDER in run.py: {app.config.get('MAIL_DEFAULT_SENDER')}")
+
 
 # Run the app
 if __name__ == '__main__':
